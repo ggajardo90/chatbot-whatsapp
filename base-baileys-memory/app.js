3924f52213f7152c96e5bacc6b80867fd49dcb10
ggajardo90 <ggajardo90@gmail.com>
2024-05-19 21:59:52 -0400

    configuracion menu del bot de wsp

--- base-baileys-memory/app.js
+++ base-baileys-memory/app.js
@@ -4,58 +4,87 @@ const QRPortalWeb = require('@bot-whatsapp/portal')
 const BaileysProvider = require('@bot-whatsapp/provider/baileys')
 const MockAdapter = require('@bot-whatsapp/database/mock')
 const { delay } = require('@whiskeysockets/baileys')
+const path = require("path")
+const fs = require("fs")
+
+const menuPath = path.join(__dirname, "mensajes", "menu.txt")
+const menu = fs.readFileSync(menuPath, "utf8")
+
+const flowMenu = addKeyword(EVENTS.ACTION)
+    .addAnswer('ingresa el numero de orden VTR', {capture:true}, async (ctx, ctxFn) =>{
+        if (ctx.body  == ctx.body) {
+            await ctxFn.flowDynamic ("favor ingresar numeros de series instalados")
+        }
+    } )
 
 
-const flowPrincipal = addKeyword(['hola', 'ole', 'alo', 'ola'])
-    .addAnswer('Buenas como estas?')
-    .addAnswer('Bienvenido a este curso')
+const flowReservas = addKeyword(EVENTS.ACTION)
+    .addAnswer('ingresa el numero de orden CLARO')
+
+
+const flowConsultas = addKeyword(EVENTS.ACTION)
+    .addAnswer('ingresa el numero de orden DIRECTV')
+
+
 
 const flowWelcome = addKeyword(EVENTS.WELCOME)
-    .addAnswer("este es el flujo Welcom", {
-        delay: 100,
-    },
-    async (ctx, ctxFn) => {
-        if (ctx.body.includes("casas")){
-            await ctxFn.flowDynamic("escribiste casas")
-        }else{
-            await ctxFn.flowDynamic("escribiste otra cosa")
-        }
-       
-    })
+    .addAnswer("Bienvenido a tu asistente virtual 🤖 XR3 insgresa tu rut", { capture: true },
+        async (ctx, ctxFn) => {
 
-    const menuFlow = addKeyword("Menu").addAnswer(
-        "Este es el menu, escogi una opcion 1,2,3,4,5, o 0",
-        { capture: true },
-        async (ctx, { gotoFlow, fallBack, flowDynamic }) => {
-            if (!["1", "2", "3", "4", "5", "0"].includes(ctx.body)) {
-                return fallBack(
-                    "Respuesta no valida, por favor selecciona una de las opciones."
-                );
+            if (ctx.body == ctx.body) {
+               return ctxFn.gotoFlow(menuFlow)
             }
+          console.log(ctx.body);
+        })
     
-            switch (ctx.body) {
-                case "1":
-                    return await flowDynamic("esta es la opcion 1")
-                case "2":
-                    return await flowDynamic("esta es la opcion 2")
-                case "3":
-                    return await flowDynamic("esta es la opcion 3")
-                case "4":
-                    return await flowDynamic("esta es la opcion 4")
-                case "5":
-                    return await flowDynamic("esta es la opcion 5")
-                case "0":
-                    return await flowDynamic(
-                        "Saliendo... Puedes volver a acceder a este menu escribiendo '*Menu*'"
-                        );
-    
-            }
+
+const flowe = addKeyword(EVENTS.WELCOME)
+    .addAnswer("escribe menu", {
+        delay: 100,
+    })
+// ,
+//     async (ctx, ctxFn) => {
+//         if (ctx.body.includes("casas")) {
+//             await ctxFn.flowDynamic("escribiste casas")
+//         } else {
+//             await ctxFn.flowDynamic("escribiste otra cosa")
+//         }
+
+//     })
+
+
+const menuFlow = addKeyword("Menu").addAnswer(
+    menu,
+    { capture: true },
+    async (ctx, { gotoFlow, fallBack, flowDynamic }) => {
+        if (!["1", "2", "3", "0"].includes(ctx.body)) {
+            return fallBack(
+                "Respuesta no valida, por favor selecciona una de las opciones."
+            );
         }
-    )
-    
+
+        switch (ctx.body) {
+            case "1":
+                return gotoFlow(flowMenu)
+            case "2":
+                return gotoFlow(flowReservas)
+            case "3":
+                return gotoFlow(flowConsultas)
+            case "0":
+                return await flowDynamic(
+                    "Saliendo... Puedes volver a acceder a este menu escribiendo '*Menu*'"
+                );
+
+        }
+    }
+)
+
+
+
+
 const main = async () => {
     const adapterDB = new MockAdapter()
-    const adapterFlow = createFlow([flowPrincipal, flowWelcome,menuFlow])
+    const adapterFlow = createFlow([flowWelcome, menuFlow, flowMenu, flowReservas, flowConsultas, flowe])
     const adapterProvider = createProvider(BaileysProvider)
 
     createBot({
